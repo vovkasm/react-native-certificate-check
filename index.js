115749e3cd0c9cfc1ce4756ba6ab483bb2e9e75e
Vladimir Timofeev <vovkasm@gmail.com>
2017-12-16 15:17:11 +0300

    fix: return meaningful error if native part not linked properly

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { NativeModules } from 'react-native'
 
 function validate(cert) {
-  return NativeModules.TCertChecker.validateCertificate(cert)
+  const native = NativeModules.TCertChecker
+  if (!native) {
+    return Promise.reject('Native module TCertChecher not found. May be you need react-native link.')
+  }
+  return native.validateCertificate(cert)
 }
 
 export default validate
